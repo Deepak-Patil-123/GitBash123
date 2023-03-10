942b1cbbbc9474a9fa7effcafe567cfade641d60
Deepak Patil <patilrhdeepak@gmail.com>
2023-03-11 00:33:07 +0530

    first time addition

---
+++ src/test/java/com/gitPractise/GitPractise.java
@@ -0,0 +1,10 @@
+package com.gitPractise;
+
+public class GitPractise {
+
+	public static void main(String[] args) {
+
+		System.out.println("git hub learning!");
+		System.out.println("program modified!");
+	}
+}
